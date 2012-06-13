9905bbd02dbb507cbda7c9968f51552748149311
atwork <atwork@BRICS-HW-Server.(none)>
2012-06-13 16:38:35 +0200

    finalize base test

--- raw_manipulation/raw_base_placement/scripts/test_base_placement.py
+++ raw_manipulation/raw_base_placement/scripts/test_base_placement.py
@@ -19,8 +19,8 @@ def main():
 
     sss.move("arm", "flatposition")
 
-    '''
     #enable object perception
+    print "enable perception"
     object_finder_srv_start = rospy.ServiceProxy('/raw_perception/object_segmentation/start', std_srvs.srv.Empty)
     rospy.wait_for_service('/raw_perception/object_segmentation/start', 30)
 
@@ -33,56 +33,54 @@ def main():
 
 
     #getb object list
-
+    print "get object list"
     object_finder_srv = rospy.ServiceProxy('/raw_perception/object_segmentation/get_segmented_objects', raw_srvs.srv.GetObjects)    
     rospy.wait_for_service('/raw_perception/object_segmentation/get_segmented_objects', 30)
-    
-    rospy.sleep(3)
+        
         
     for i in range(20): 
         print "find object try: ", i
         resp = object_finder_srv()
-          
+
         if (len(resp.objects) <= 0):
             rospy.loginfo('found no objects')
-            rospy.slerespep(1);
-        else:    
+            rospy.sleep(1);
+        else:
             rospy.loginfo('found {0} objects'.format(len(resp.objects)))
             break
-    '''
+    
+
+    if (len(resp.objects) <= 0):
+        rospy.loginfo('found NO objects')
+        return
 
     # BASE PLACEMENT
     base_placement_srv = rospy.ServiceProxy('/raw_base_placement/calculateOptimalBasePose', raw_srvs.srv.GetPoseStamped) 
     print "wait for service: /raw_base_placement/calculateOptimalBasePose"   
     rospy.wait_for_service('/raw_base_placement/calculateOptimalBasePose', 30)
 
-    #    for object in resp.objects:                   
-    #       if object.pose.pose.position.z <= 0.05 and object.pose.pose.position.z >= 0.30:
-    #          continue
+
+    for object_item in resp.objects:                   
+    	if object_item.pose.pose.position.z <= 0.05 and object_item.pose.pose.position.z >= 0.30:
+    	    continue
     
-    object_pose = geometry_msgs.msg.PoseStamped()
+        object_pose = object_item.pose
 
-    object_pose.header.frame_id = "base_link"
-    object_pose.header.stamp = rospy.Time.now()
-    object_pose.pose.position.x = 0.3
-    object_pose.pose.position.y = -0.1
-    object_pose.pose.position.z = 0.1
+        print "OBJ POSE: ", object_pose
 
-    print "OBJ POSE: ", object_pose
+        #transform base_link to map
+        tf_listener.waitForTransform("/map", object_pose.header.frame_id, object_pose.header.stamp, rospy.Duration(10))
+        obj_pose_transformed = tf_listener.transformPose("map", object_pose)
 
-    #transform base_link to map
-    tf_listener.waitForTransform("/map", object_pose.header.frame_id, object_pose.header.stamp, rospy.Duration(10))
-    obj_pose_transformed = tf_listener.transformPose("map", object_pose)
-
-    print "OBJ POSE TRANSFORMED: ", object_pose_transformed
+        print "OBJ POSE TRANSFORMED: ", obj_pose_transformed
 
 
-    # call base placement service
-    base_pose = base_placement_srv(obj_pose_transformed)
+        # call base placement service
+        base_pose = base_placement_srv(obj_pose_transformed)
 
-    print "BASE_POSE", base_pose
+        print "BASE_POSE", base_pose
    
-    '''
+    
     #stop object perception
     object_finder_srv_stop = rospy.ServiceProxy('/raw_perception/object_segmentation/stop', std_srvs.srv.Empty)
     rospy.wait_for_service('/raw_perception/object_segmentation/stop', 5)
@@ -93,7 +91,11 @@ def main():
         error_message = "%s"%e
         rospy.logerr("calling <</raw_perception/object_segmentation/stop>> service not successfull, error: %s", error_message)
         return 
-    '''
+
+
+    ## DO MOVEMENT TO CALCULATED BASE POSE
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([base_pose.base_pose.pose.orientation.x, base_pose.base_pose.pose.orientation.y, base_pose.base_pose.pose.orientation.z, base_pose.base_pose.pose.orientation.w])
+    sss.move("base", [base_pose.base_pose.pose.position.x, base_pose.base_pose.pose.position.y, yaw])
 
 if __name__ == '__main__':
     main()
