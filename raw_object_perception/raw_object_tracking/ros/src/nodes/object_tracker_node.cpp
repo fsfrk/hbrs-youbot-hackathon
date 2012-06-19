d76eeb88e29308abf331cf85617ef3ec5380cbbf
Sergey Alexandrov <alexandrov88@gmail.com>
2012-06-19 14:08:33 -0500

    Fix bug with fpring formatting. Finally.

--- raw_object_perception/raw_object_tracking/ros/src/nodes/object_tracker_node.cpp
+++ raw_object_perception/raw_object_tracking/ros/src/nodes/object_tracker_node.cpp
@@ -63,7 +63,7 @@ public:
     tce_->setInputCloud(cloud);
     tce_->setTablePolygon(planar_polygon_);
     tce_->extract(clusters);
-    ROS_INFO("Number of clusters: %ui.", clusters.size());
+    ROS_INFO_STREAM("Number of clusters: " << clusters.size());
 
     for (const PointCloud::Ptr& cluster : clusters)
       tracker_->addCluster(cluster);
