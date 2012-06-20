23f302bb9d16f63b7b182613bf270bfe7806a2f4
Alex Moriarty <alexander@dal.ca>
2012-06-20 00:00:04 -0500

    bunch-O-simple arm commands, tested

--- raw_manipulation/raw_arm_navigation/ros/scripts/arm_cmd/move_arm_to_pregrasp.py
+++ raw_manipulation/raw_arm_navigation/ros/scripts/arm_cmd/move_arm_to_pregrasp.py
@@ -8,14 +8,9 @@ class move_arm_to_pregrasp:
 	def __init__(self):
 		
 	def move(self, grasp_name):
-		
-		
-	
 
 def main():
 	rospy.init_node("move_arm_to_pregrasp")
-
-	
 
 if __name__ == "__main__":
 	main()
--- raw_manipulation/raw_arm_navigation/ros/scripts/arm_cmd/to_zero_pose.py
+++ raw_manipulation/raw_arm_navigation/ros/scripts/arm_cmd/to_zero_pose.py
@@ -2,7 +2,7 @@
 import time
 
 import roslib
-roslib.load_manifest('raw_script_server')
+roslib.load_manifest('raw_arm_navigation')
 import rospy
 import actionlib
 
@@ -12,9 +12,8 @@ from simple_script_server import *
 sss = simple_script_server()
 
 def main():
-    rospy.init_node('script_server_test')
-    sss.move("arm", "pregrasp_laying_mex")
-    sss.move("gripper", "open")
+    rospy.init_node('move_to_zero')
+    sss.move("arm", "zeroposition")
     rospy.sleep(1)
 
 ## Main routine for running the script server
