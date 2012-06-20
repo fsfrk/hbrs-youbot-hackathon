2bfb8d0e2b556497fd56e8d4680e7b3f5e1d0fb4
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-06-20 08:02:56 -0500

    continue work on BMT state machine

--- raw_scenarios/raw_basic_manipulation_test_wc2012/scripts/basic_manipulation_test.py
+++ raw_scenarios/raw_basic_manipulation_test_wc2012/scripts/basic_manipulation_test.py
@@ -9,6 +9,8 @@ import smach_ros
 from generic_robocup_states import *
 from generic_navigation_states import *
 from generic_manipulation_states import *
+from generic_perception_states import *
+
 from basic_manipulation_test_wc2012_states import *
 
 # main
@@ -19,7 +21,12 @@ def main():
     
     # world knowledge
     SM.userdata.task_spec = 0
+    
     SM.userdata.base_pose_to_approach = 0
+    
+    SM.userdata.recognized_objects = []
+    SM.userdata.object_to_grasp = 0
+    
     SM.userdata.rear_platform_free_poses = ['pltf_pose_1', 'pltf_pose_2', 'pltf_pose_3']
     SM.userdata.rear_platform_occupied_poses = []
     
@@ -46,53 +53,57 @@ def main():
                         'failed':'ADJUST_POSE_WRT_PLATFORM'})
 
         smach.StateMachine.add('MOVE_ARM_OUT_OF_VIEW', move_arm_out_of_view(),
+            transitions={'succeeded':'ENABLE_PERCEPTION',
+                        'failed':'MOVE_ARM_OUT_OF_VIEW'})
+        
+        smach.StateMachine.add('ENABLE_PERCEPTION', enable_object_finder(),
+                transitions={'succeeded':'RECOGNIZE_OBJECTS',  
+                             'failed':'ENABLE_PERCEPTION'})
+        
+        #ToDo: implement state
+        smach.StateMachine.add('RECOGNIZE_OBJECTS', recognize_objects(),
+            transitions={'succeeded':'SELECT_RECOGNIZED_OBJECT',
+                        'failed':'RECOGNIZE_OBJECTS'})
+        
+        smach.StateMachine.add('SELECT_RECOGNIZED_OBJECT', select_recognized_object(),
+            transitions={'succeeded':'PLACE_BASE_IN_FRONT_OF_OBJECT',
+                        'no_more_objects':'SELECT_DESTINATION_POSE'})
+        
+        #ToDo: implement state
+        smach.StateMachine.add('PLACE_BASE_IN_FRONT_OF_OBJECT', adjust_pose_wrt_recognized_obj(),
+            transitions={'succeeded':'overall_success',
+                        'failed':'PLACE_BASE_IN_FRONT_OF_OBJECT'})
+
+        '''
+        smach.StateMachine.add('GRASP_OBJ_WITH_VISUAL_SERVERING', grasp_obj_with_visual_servering(),
             transitions={'succeeded':'overall_success',
                         'failed':'MOVE_ARM_OUT_OF_VIEW'})
-
-        '''        
-        smach.StateMachine.add('SELECT_OBJ_TO_BE_RECOGNIZED', select_obj_to_be_recognized(),
-            transitions={'object_selected':'RECOGNIZE_OBJECTS'},
-                         'no_more_objects':'SELECT_DESTINATION_POSE')
-        
-        smach.StateMachine.add('RECOGNIZE_OBJECTS', recognize_objects(),
-            transitions={'succeeded':'PLACE_BASE_IN_FRONT_OF_OBJECT'},
-                        {'failed':'RECOGNIZE_OBJECTS'})
-         
-        smach.StateMachine.add('PLACE_BASE_IN_FRONT_OF_OBJECT', place_base_in_front_of_object(),
-            transitions={'succeeded':'PLACE_IN_FRONT_OF_OBJECT'},
-                        {'failed':'PLACE_BASE_IN_FRONT_OF_OBJECT'})
-
-        smach.StateMachine.add('PLACE_IN_FRONT_OF_OBJECT', place_base_in_front_of_object(),
-            transitions={'succeeded':'overall_success'},
-                        {'failed':'PLACE_IN_FRONT_OF_OBJECT'})
-        
-        smach.StateMachine.add('GRASP_OBJ_WITH_VISUAL_SERVERING', grasp_obj_with_visual_servering(),
-            transitions={'succeeded':'overall_success'},
-                        {'failed':'MOVE_ARM_OUT_OF_VIEW'})
         
         smach.StateMachine.add('PLACE_OBJ_ON_REAR_PLATFORM', place_obj_on_rear_platform(),
-            transitions={'succeeded':'SELECT_OBJ_TO_BE_RECOGNIZED'},
-                        {'failed':'PLACE_OBJ_ON_REAR_PLATFORM'})
+            transitions={'succeeded':'SELECT_OBJ_TO_BE_RECOGNIZED',
+                        'failed':'PLACE_OBJ_ON_REAR_PLATFORM'})
+        '''
         
         
         # go to the destination pose and place the objects in the desired configuration on the platform
         smach.StateMachine.add('SELECT_DESTINATION_POSE', select_pose("destination_pose"),
             transitions={'succeeded':'MOVE_TO_DESTINATION_POSE'})
         
-        smach.StateMachine.add('APPROACH_DESTINATION_POSE', approach_pose(),
-            transitions={'succeeded':'overall_success'},
-                        {'failed':'APPROACH_SOURCE_POSE'})
+        smach.StateMachine.add('MOVE_TO_DESTINATION_POSE', approach_pose(),
+            transitions={'succeeded':'overall_success',
+                        'failed':'overall_success'})
         
+        '''
         smach.StateMachine.add('GRASP_OBJECT_FROM_PLTF', grasp_obj_from_pltf(),
-            transitions={'succeeded':'overall_success'},
-                        {'failed':'APPROACH_SOURCE_POSE'})
+            transitions={'succeeded':'overall_success',
+                        'failed':'APPROACH_SOURCE_POSE'})
         
         smach.StateMachine.add('COMPUTE_OBJ_GOAL_POSITIONS', compute_obj_goal_positions(),
             transitions={'succeeded':'SELECT_OBJ_GOAL_POSE'})
         
         smach.StateMachine.add('SELECT_OBJ_GOAL_POSE', select_obj_goal_pose(),
-            transitions={'succeeded':'GRASP_OBJ_FROM_PLTF'}
-                        {'no_more_objects':'MOVE_TO_EXIT'})
+            transitions={'succeeded':'GRASP_OBJ_FROM_PLTF'
+                        'no_more_objects':'MOVE_TO_EXIT'})
         
         smach.StateMachine.add('GRASP_OBJ_FROM_PLTF', grasp_obj_from_pltf(),
             transitions={'succeeded':'PLACE_OBJ_ON_GOAL_POSE'})
