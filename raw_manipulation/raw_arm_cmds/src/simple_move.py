4a5e0d77325cca89559430f45d96a9d5d548b4ef
Alex Moriarty <alexander@dal.ca>
2012-06-22 15:46:11 -0500

    new pre grasps for platform

--- raw_manipulation/raw_arm_cmds/src/simple_move.py
+++ raw_manipulation/raw_arm_cmds/src/simple_move.py
@@ -20,27 +20,27 @@ if __name__=="__main__":
         
         jv1 = JointValue()
         jv1.joint_uri = "arm_joint_1"
-        jv1.value = 0.01
+        jv1.value = 0.011
         jv1.unit = "rad"
         
         jv2 = JointValue()
         jv2.joint_uri = "arm_joint_2"
-        jv2.value = 0.01
+        jv2.value = 0.011
         jv2.unit = "rad"
 
         jv3 = JointValue()
         jv3.joint_uri = "arm_joint_3"
-        jv3.value = -0.01
+        jv3.value = -0.016
         jv3.unit = "rad"
         
         jv4 = JointValue()
         jv4.joint_uri = "arm_joint_4"
-        jv4.value = 0.01
+        jv4.value = 0.023
         jv4.unit = "rad"
         
         jv5 = JointValue()
         jv5.joint_uri = "arm_joint_5"
-        jv5.value = 0.01
+        jv5.value = 0.12
         jv5.unit = "rad"
         
 
@@ -55,5 +55,16 @@ if __name__=="__main__":
         
     except Exception, e:
         print e
-
-
+'''
+JOINT LIMITS:
+[ WARN] [1340337373.846618553]: Cannot set arm joint 1: 
+ The setpoint angle is out of range. The valid range is between 0.0100692 rad and 5.84014 rad and it is: 0.01 rad
+[ WARN] [1340337373.847057559]: Cannot set arm joint 2: 
+ The setpoint angle is out of range. The valid range is between 0.0100692 rad and 2.61799 rad and it is: 0.01 rad
+[ WARN] [1340337373.847447567]: Cannot set arm joint 3: 
+ The setpoint angle is out of range. The valid range is between -5.02655 rad and -0.015708 rad and it is: -0.01 rad
+[ WARN] [1340337373.853084080]: Cannot set arm joint 4: 
+ The setpoint angle is out of range. The valid range is between 0.0221239 rad and 3.4292 rad and it is: 0.01 rad
+[ WARN] [1340337373.854206454]: Cannot set arm joint 5: 
+ The setpoint angle is out of range. The valid range is between 0.110619 rad and 5.64159 rad and it is: 0.01 rad
+'''
