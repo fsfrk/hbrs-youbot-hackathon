20268027f512c29542b30a37d7ab6cc0d9ec9345
Praveen <praman2s@youbot.(none)>
2012-09-07 19:24:25 +0200

    Added robot pose with respect to map

---
+++ raw_navigation/raw_2dnav_iros2012/src/navigation.cpp
@@ -0,0 +1,33 @@
+//============================================================================
+// Name        : Navigation module
+// Author      : Praveen Ramanujam
+// Version     :
+// Copyright   : Your copyright notice
+// Description :
+//============================================================================
+
+#include <iostream>
+#include "raw_2dnav_iros2012/common.h"
+#include "raw_2dnav_iros2012/robotpose.hpp"
+
+
+int main(int argc, char** argv) {
+
+	ros::init(argc, argv, "tf_");
+	ros::NodeHandle node;
+	double yaw;
+	tf::TransformListener tf(ros::Duration(10));
+	RobotPose pose;
+	geometry_msgs::Pose robotpose;
+	ros::Rate rate(10.0);
+	while (ros::ok())
+	{
+		//Get Current Robot Pose
+	     robotpose = pose.getRobotPose(yaw);
+	     cout << "[" << robotpose.position.x <<"  "<< robotpose.position.y <<"  "<<yaw<<"]" << endl;
+	     rate.sleep();
+	}
+
+	return 0;
+}
+
