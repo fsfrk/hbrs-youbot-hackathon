8387cd4b88c4b9b6549334d60e761d141ad24aa9
Nemo Giftsun <nirmalgibbs@gmail.com>
2012-09-20 10:07:40 +0200

    Changes to afford even if the odometry messages are received

--- raw_manipulation/raw_relative_movements/scripts/test_base_placement.py
+++ raw_manipulation/raw_relative_movements/scripts/test_base_placement.py
@@ -21,16 +21,20 @@ def main():
     ##sss.move("arm", "pregrasp_front_init")
     
     # BASE PLACEMENT
-    shiftbase_srv = rospy.ServiceProxy('/raw_relative_movements/shiftbase', raw_srvs.srv.SetPoseStamped) 
+    ##shiftbase_srv = rospy.ServiceProxy('/raw_relative_movements/shiftbase', raw_srvs.srv.SetPoseStamped)
+    moveoptimalbase_srv = rospy.ServiceProxy('/raw_relative_movements/movetooptimalbase', raw_srvs.srv.SetPoseStamped) 
     ##moveoptimalbase_srv = rospy.ServiceProxy('/raw_base_placement_nirmal/moveoptimalbase', raw_srvs.srv.SetPoseStamped) 
 
-    print "wait for service: /raw_relative_movements/shiftbase"   
-    rospy.wait_for_service('/raw_relative_movements/shiftbase', 30)
+    ##print "wait for service: /raw_relative_movements/shiftbase"   
+    ##rospy.wait_for_service('/raw_relative_movements/shiftbase', 30)
+
+    print "wait for service: /raw_relative_movements/moveooptimalbase"   
+    rospy.wait_for_service('/raw_relative_movements/movetooptimalbase', 30)
 
     goalpose = geometry_msgs.msg.PoseStamped()
-    goalpose.pose.position.x = -0.3
-    goalpose.pose.position.y = -0.1
-    goalpose.pose.position.z = 0.0
+    goalpose.pose.position.x = 0.1
+    goalpose.pose.position.y = 0.1
+    goalpose.pose.position.z = 0.1
     quat = tf.transformations.quaternion_from_euler(0,0,0)
     goalpose.pose.orientation.x = quat[0]
     goalpose.pose.orientation.y = quat[1]
@@ -39,8 +43,21 @@ def main():
     
     print "GOAL POSE TRANSFORMED: ", goalpose
     # call base placement service
-    base_pose = shiftbase_srv(goalpose)  
+    base_pose = moveoptimalbase_srv(goalpose)  
 
+    goalpose = geometry_msgs.msg.PoseStamped()
+    goalpose.pose.position.x = -0.1
+    goalpose.pose.position.y = -0.1
+    goalpose.pose.position.z = 0.1
+    quat = tf.transformations.quaternion_from_euler(0,0,0)
+    goalpose.pose.orientation.x = quat[0]
+    goalpose.pose.orientation.y = quat[1]
+    goalpose.pose.orientation.z = quat[2]
+    goalpose.pose.orientation.w = quat[3]
+    
+    print "GOAL POSE TRANSFORMED: ", goalpose
+    # call base placement service
+    base_pose = moveoptimalbase_srv(goalpose) 
 
 if __name__ == '__main__':
     main()
