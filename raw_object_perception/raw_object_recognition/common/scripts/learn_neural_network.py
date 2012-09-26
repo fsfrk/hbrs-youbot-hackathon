81ce59f273d935bab3053a51b67344e50dcb759d
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-26 19:36:17 +0200

    Add JointDataset class for combining e.g. 'standard' and 'standing' objects data.

--- raw_object_perception/raw_object_recognition/common/scripts/learn_neural_network.py
+++ raw_object_perception/raw_object_recognition/common/scripts/learn_neural_network.py
@@ -7,24 +7,29 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules import SoftmaxLayer
 
-from dataset import Dataset
+from dataset import Dataset, JointDataset
 
 if __name__ == '__main__':
-    dataset = Dataset('../data/', 'standard')
+    d1 = Dataset('../data/', 'standard')
+    d2 = Dataset('../data/', 'standing')
+    dataset = JointDataset([d1, d2])
     X, Y, labels = dataset.load('all')
     mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
     features = len(mean)
     X -= mean
+    X /= std
     cds = ClassificationDataSet(features, class_labels=labels)
     for x, y in zip(X, Y):
         cds.addSample(x, y)
+    print cds.calculateStatistics()
     tstdata, trndata = cds.splitWithProportion(0.2)
     trndata._convertToOneOfMany()
     tstdata._convertToOneOfMany()
-    nn = buildNetwork(trndata.indim, 16, trndata.outdim, outclass=SoftmaxLayer)
+    nn = buildNetwork(trndata.indim, 12, trndata.outdim, outclass=SoftmaxLayer)
     trainer = BackpropTrainer(nn, dataset=trndata, momentum=0.05, verbose=True,
                               weightdecay=0.01)
-    trainer.trainUntilConvergence(maxEpochs=500)
+    trainer.trainUntilConvergence(maxEpochs=300)
     trnr = percentError(trainer.testOnClassData(), trndata['class'])
     tstr = percentError(trainer.testOnClassData(dataset=tstdata),
                         tstdata['class'])
