440a3c5747f6beec47a3d1803f3873e533247a01
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-27 18:47:09 +0200

    Add helper class to manage pybrains's neural network.

---
+++ raw_object_perception/raw_object_recognition/common/src/classification_network.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import yaml
+import numpy as np
+
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
+
+
+class ClassificationNetwork:
+    def __init__(self, net, labels, mean, std):
+        self.net = net
+        self.labels = labels
+        self.mean = mean
+        self.std = std
+
+    def save(self, filename, desc=None):
+        NetworkWriter.writeToFile(self.net, filename + '.xml')
+        params = {'labels': self.labels,
+                  'mean': self.mean.tolist(),
+                  'std': self.std.tolist()}
+        with open(filename + '.yaml', 'w') as f:
+            f.write(yaml.dump(params, default_flow_style=False))
+
+    @classmethod
+    def load(cls, filename):
+        net = NetworkReader.readFrom(filename + '.xml')
+        params = yaml.load(open(filename + '.yaml', 'r'))
+        mean = np.array(params['mean'])
+        std = np.array(params['std'])
+        return ClassificationNetwork(net, params['labels'], mean, std)
