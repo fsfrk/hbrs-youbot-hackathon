1cf3dc72b998c681ed7e54b2cb800be8b71b4570
Sergey Alexandrov <alexandrov88@gmail.com>
2012-09-28 12:28:20 +0200

    Update train network script to use new helper classes. Add command-line parameters to control the training dataset.

--- raw_object_perception/raw_object_recognition/ros/scripts/train_classification_network.py
+++ raw_object_perception/raw_object_recognition/ros/scripts/train_classification_network.py
@@ -1,5 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
+PACKAGE = 'raw_object_recognition'
+
+import roslib
+roslib.load_manifest(PACKAGE)
+import sys
+from os.path import join
+import argparse
+
+# Import helper classes for managing training data and saving network
+sys.path.append(join(roslib.packages.get_pkg_dir(PACKAGE), 'common', 'src'))
+from dataset import Dataset, JointDataset
+from classification_network import ClassificationNetwork
+
+# Import numpy and pybrain tools
 import numpy as np
 from pybrain.datasets import ClassificationDataSet
 from pybrain.utilities import percentError
@@ -7,19 +21,35 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules import SoftmaxLayer
 
-from dataset import Dataset, JointDataset
+
+def to_string(np_array):
+    return str(np_array.tolist())
 
 if __name__ == '__main__':
-    d1 = Dataset('../data/', 'standard')
-    d2 = Dataset('../data/', 'standing')
-    dataset = JointDataset([d1, d2])
-    X, Y, labels = dataset.load('all')
+    parser = argparse.ArgumentParser(description='''
+    Learn and save neural network for object recognition.
+    The network will be saved to the "common/config/" folder.
+    ''')
+    parser.add_argument('--datasets', help='list of datasets to use',
+                        nargs='*', default='standard')
+    parser.add_argument('--objects', help='list of objects to use',
+                        nargs='*', default='all')
+    parser.add_argument('--output', help='output filename (without folder and '
+                        'extension, default: "network")', default='network')
+    args = parser.parse_args()
+    data_folder = join(roslib.packages.get_pkg_dir(PACKAGE), 'common', 'data')
+    cfg_folder = join(roslib.packages.get_pkg_dir(PACKAGE), 'common', 'config')
+    if isinstance(args.datasets, list):
+        datasets = [Dataset(data_folder, name) for name in args.datasets]
+        dataset = JointDataset(datasets)
+    else:
+        dataset = Dataset(data_folder, args.datasets)
+    X, Y, labels = dataset.load(args.objects)
     mean = np.mean(X, axis=0)
     std = np.std(X, axis=0)
-    features = len(mean)
     X -= mean
     X /= std
-    cds = ClassificationDataSet(features, class_labels=labels)
+    cds = ClassificationDataSet(len(mean), class_labels=labels)
     for x, y in zip(X, Y):
         cds.addSample(x, y)
     print cds.calculateStatistics()
@@ -29,9 +59,11 @@ if __name__ == '__main__':
     nn = buildNetwork(trndata.indim, 12, trndata.outdim, outclass=SoftmaxLayer)
     trainer = BackpropTrainer(nn, dataset=trndata, momentum=0.05, verbose=True,
                               weightdecay=0.01)
-    trainer.trainUntilConvergence(maxEpochs=300)
+    trainer.trainUntilConvergence(maxEpochs=400)
     trnr = percentError(trainer.testOnClassData(), trndata['class'])
     tstr = percentError(trainer.testOnClassData(dataset=tstdata),
                         tstdata['class'])
-    print "Epochs: %4d, train error: %5.2f%%, test error: %5.2f%%" % (
+    print 'Epochs: %4d, train error: %5.2f%%, test error: %5.2f%%' % (
         trainer.totalepochs, trnr, tstr)
+    ClassificationNetwork(nn, labels, mean, std).save(join(cfg_folder,
+                                                           args.output))
