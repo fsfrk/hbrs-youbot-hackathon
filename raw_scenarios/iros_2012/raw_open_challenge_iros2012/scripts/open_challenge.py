59740896448815de54013557e44218d90bf8c711
Jose Aguilar <joseaguilar13@gmail.com>
2012-09-28 19:29:06 -0500

    Added open_challenge_iros_states and open_challenge state machine. To Do specify the string name from the recognized objects to sss.play

--- raw_scenarios/iros_2012/raw_open_challenge_iros2012/scripts/open_challenge.py
+++ raw_scenarios/iros_2012/raw_open_challenge_iros2012/scripts/open_challenge.py
@@ -7,6 +7,15 @@ import smach_ros
 
 # import of generic states
 #from generic_robocup_states import *
+rom generic_basic_states import *
+from generic_robocup_states import *
+from generic_navigation_states import *
+from generic_manipulation_states import *
+from generic_perception_states import *
+
+from basic_manipulation_test_iros2012_states import *
+from open_challenge_test_iros2012_states import *
+
 
 
 # main
@@ -18,12 +27,107 @@ def main():
     # world knowledge
     #SM.userdata.task_spec = 0
     
+    SM.userdata.task_spec = 0
+    
+    SM.userdata.base_pose_to_approach = 0
+    
+    SM.userdata.recognized_objects = []
+    SM.userdata.object_to_grasp = 0
+    
+    SM.userdata.rear_platform_free_poses = ['platform_right', 'platform_centre', 'platform_left']
+    SM.userdata.rear_platform_occupied_poses = []
+    
+    SM.userdata.obj_goal_configuration_poses = []
     
     # open the container
     with SM:
         # add states to the container
         
-     
+   # move to the source pose, recognize objects, grasp them and put them on the rear platform
+        smach.StateMachine.add('GET_TASK', get_basic_manipulation_task(),
+            transitions={'task_received':'INIT_ROBOT', 
+                         'wront_task_format':'GET_TASK'})
+        
+        smach.StateMachine.add('INIT_ROBOT', init_robot(),
+            transitions={'succeeded':'SELECT_SOURCE_POSE'})
+
+        smach.StateMachine.add('SELECT_SOURCE_POSE', select_base_pose("source_pose"),
+            transitions={'succeeded':'APPROACH_SOURCE_POSE'})
+        
+        smach.StateMachine.add('APPROACH_SOURCE_POSE', approach_pose(),
+            transitions={'succeeded':'ADJUST_POSE_WRT_PLATFORM',
+                        'failed':'APPROACH_SOURCE_POSE'})
+	
+        smach.StateMachine.add('ADJUST_POSE_WRT_PLATFORM', adjust_pose_wrt_platform(),
+            transitions={'succeeded':'MOVE_ARM_OUT_OF_VIEW',
+                        'failed':'ADJUST_POSE_WRT_PLATFORM'})
+
+        smach.StateMachine.add('MOVE_ARM_OUT_OF_VIEW', move_arm_out_of_view(),
+            transitions={'succeeded':'RECOGNIZE_OBJECTS'})
+                
+        smach.StateMachine.add('RECOGNIZE_OBJECTS', recognize_objects(),
+            transitions={'succeeded':'SELECT_RECOGNIZED_OBJECT',
+                        'failed':'overall_failed'})
+
+        smach.StateMachine.add('POINT TO RECOGNIZED OBJECTS', point_to_recognized_objects(),
+            transitions={'succeeded':'SELECT_FINAL_POSE',
+                        'failed':'overall_failed'})
+        
+      '''  smach.StateMachine.add('SELECT_RECOGNIZED_OBJECT', select_recognized_object(),
+            transitions={'succeeded':'PLACE_BASE_IN_FRONT_OF_OBJECT',
+                        'no_more_objects':'SELECT_DESTINATION_POSE'})
+        
+        smach.StateMachine.add('PLACE_BASE_IN_FRONT_OF_OBJECT', adjust_pose_wrt_recognized_obj(),
+            transitions={'succeeded':'GRASP_OBJ_WITH_VISUAL_SERVERING',
+                        'failed':'PLACE_BASE_IN_FRONT_OF_OBJECT'})     
+
+        smach.StateMachine.add('GRASP_OBJ_WITH_VISUAL_SERVERING', grasp_obj_with_visual_servering(),
+            transitions={'succeeded':'PLACE_OBJ_ON_REAR_PLATFORM',
+                        'failed':'GRASP_OBJ_WITH_VISUAL_SERVERING'})
+        
+        smach.StateMachine.add('PLACE_OBJ_ON_REAR_PLATFORM', place_obj_on_rear_platform(),
+            transitions={'succeeded':'SELECT_RECOGNIZED_OBJECT',
+                        'no_more_free_poses':'SELECT_DESTINATION_POSE'})
+      
+
+        # go to the destination pose and place the objects in the desired configuration on the platform
+        smach.StateMachine.add('SELECT_DESTINATION_POSE', select_base_pose("destination_pose"),
+            transitions={'succeeded':'APPROACH_DESTINATION_POSE'})
+        
+        smach.StateMachine.add('APPROACH_DESTINATION_POSE', approach_pose(),
+            transitions={'succeeded':'ADJUST_POSE_WRT_PLATFORM2',
+                        'failed':'APPROACH_DESTINATION_POSE'})
+
+        smach.StateMachine.add('ADJUST_POSE_WRT_PLATFORM2', adjust_pose_wrt_platform(),
+            transitions={'succeeded':'GET_OBJ_POSES_FOR_CONFIGURATION',
+                        'failed':'ADJUST_POSE_WRT_PLATFORM2'})
+        
+        smach.StateMachine.add('GET_OBJ_POSES_FOR_CONFIGURATION', get_obj_poses_for_goal_configuration(),
+            transitions={'succeeded':'GRASP_OBJECT_FROM_PLTF',
+                         'configuration_poses_not_available':'overall_failed'})
+        
+        smach.StateMachine.add('GRASP_OBJECT_FROM_PLTF', grasp_obj_from_pltf(),
+            transitions={'succeeded':'PLACE_OBJ_IN_CONFIGURATION',
+                        'no_more_obj_on_pltf':'SELECT_FINAL_POSE'})
+        
+        smach.StateMachine.add('PLACE_OBJ_IN_CONFIGURATION', place_object_in_configuration(),
+            transitions={'succeeded':'GRASP_OBJECT_FROM_PLTF',
+                        'no_more_cfg_poses':'SELECT_FINAL_POSE'})
+        '''       
+        # if everything is done move to final pose
+        smach.StateMachine.add('SELECT_FINAL_POSE', select_base_pose("final_pose"),
+            transitions={'succeeded':'MOVE_ARM_TO_INIT'})
+
+        #smach.StateMachine.add('MOVE_ARM_TO_ZERO', move_arm("zeroposition"),
+        #    transitions={'succeeded':'MOVE_ARM_TO_INIT'})
+
+        smach.StateMachine.add('MOVE_ARM_TO_INIT', move_arm("initposition", do_blocking = False),
+            transitions={'succeeded':'APPROACH_EXIT_POSE'})
+        
+        smach.StateMachine.add('APPROACH_EXIT_POSE', approach_pose("EXIT"),
+            transitions={'succeeded':'overall_success',
+                         'failed':'APPROACH_EXIT_POSE'})
+                  
         
        
     # Start SMACH viewer
--- raw_scenarios/iros_2012/raw_open_challenge_iros2012/src/open_challenge_iros2012_states.py
+++ raw_scenarios/iros_2012/raw_open_challenge_iros2012/src/open_challenge_iros2012_states.py
@@ -8,4 +8,42 @@ import smach_ros
 from simple_script_server import *
 sss = simple_script_server()
 
+class point_to_recognized_objects(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['succeeded', 'failed'], input_keys=['object_list', 'recognized_objects'])
+
+        
+    def execute(self, userdata):
+       #sss.move("gripper", "open", blocking=False)
+       # sss.move("arm", "zeroposition")
+        
+        for object in userdata.recognized_objects:         
+            
+            # ToDo: need to be adjusted to correct stuff           
+            if object.pose.pose.position.z <= 0.0 or object.pose.pose.position.z >= 0.10:
+                continue
+    
+            global planning_mode
+            sss.move("arm", "zeroposition", mode=planning_mode)                             
+
+            #object.pose.pose.position.z = object.pose.pose.position.z + 0.02
+            object.pose.pose.position.x = object.pose.pose.position.x + 0.01
+            object.pose.pose.position.y = object.pose.pose.position.y - 0.005
+
+            #ToDo Check the 'pregrasp' position to point the object
+            handle_arm = sss.move("arm", [object.pose.pose.position.x, object.pose.pose.position.y, object.pose.pose.position.z, "/base_link"], mode=planning_mode)
+
+            if handle_arm.get_state() == 3:
+                #sss.move("gripper", "close", blocking=False)
+                #ToDo complete the sss.play with the objects names from recognized_objects and add .wav files to the directory
+                #sss.play(object.name)
+                rospy.sleep(3.0)
+                sss.move("arm", "zeroposition", mode=planning_mode)        
+                return 'succeeded'    
+            else:
+                rospy.logerr('could not find IK for current object')
+
+        return 'failed'
+
 
