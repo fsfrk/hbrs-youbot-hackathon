27678f63856e4e7e4fe9af2bf99dd351858a3dea
Frederik Hegger <frederik.hegger@inf.h-brs.de>
2012-10-08 23:45:29 +0100

    finish roughly the open challenge script

--- raw_scenarios/iros_2012/raw_open_challenge_iros2012/scripts/open_challenge.py
+++ raw_scenarios/iros_2012/raw_open_challenge_iros2012/scripts/open_challenge.py
@@ -26,12 +26,7 @@ def main():
     SM = smach.StateMachine(outcomes=['overall_success', 'overall_failed'])
     
     # world knowledge
-    #SM.userdata.task_spec = 0
-    
-    SM.userdata.task_spec = 0
-    
     SM.userdata.base_pose_to_approach = 0
-    
     SM.userdata.recognized_objects = []
     SM.userdata.object_to_grasp = 0
     
@@ -44,18 +39,10 @@ def main():
     with SM:
         # add states to the container
         
-   # move to the source pose, recognize objects, grasp them and put them on the rear platform
-        smach.StateMachine.add('GET_TASK', get_basic_manipulation_task(),
-            transitions={'task_received':'INIT_ROBOT', 
-                         'wront_task_format':'GET_TASK'})
-        
         smach.StateMachine.add('INIT_ROBOT', init_robot(),
             transitions={'succeeded':'SELECT_SOURCE_POSE'})
-
-        smach.StateMachine.add('SELECT_SOURCE_POSE', select_base_pose("source_pose"),
-            transitions={'succeeded':'APPROACH_SOURCE_POSE'})
         
-        smach.StateMachine.add('APPROACH_SOURCE_POSE', approach_pose(),
+        smach.StateMachine.add('APPROACH_SOURCE_POSE', approach_pose("S1"),
             transitions={'succeeded':'ADJUST_POSE_WRT_PLATFORM',
                         'failed':'APPROACH_SOURCE_POSE'})
 	
@@ -70,31 +57,11 @@ def main():
             transitions={'succeeded':'POINT_TO_RECOGNIZED_OBJECTS',
                         'failed':'overall_failed'})
 
-       # smach.StateMachine.add('POINT_TO_RECOGNIZED_OBJECTS', point_to_recognized_objects(),
-       #     transitions={'succeeded':'SELECT_FINAL_POSE',
-       #                 'failed':'overall_failed'})
-        
-     
-        smach.StateMachine.add('SELECT_RECOGNIZED_OBJECT', select_recognized_object(),
-            transitions={'succeeded':'PLACE_BASE_IN_FRONT_OF_OBJECT',
-                        'no_more_objects':'SELECT_FINAL_POSE'})
-        
-        smach.StateMachine.add('PLACE_BASE_IN_FRONT_OF_OBJECT', adjust_pose_wrt_recognized_obj(),
-            transitions={'succeeded':'POINT_TO_SELECTED_OBJECT',
-                        'failed':'PLACE_BASE_IN_FRONT_OF_OBJECT'})     
-
-        smach.StateMachine.add('POINT_TO_SELECTED_OBJECT', point_to_selected_object(),
-            transitions={'succeeded':'SELECT_RECOGNIZED_OBJECT',
+        smach.StateMachine.add('POINT_TO_SELECTED_OBJECT', point_to_and_announce_recognized_objects(),
+            transitions={'succeeded':'MOVE_ARM_TO_INIT',
                         'failed':'POINT_TO_SELECTED_OBJECT'})
         
             
-        # if everything is done move to final pose
-        smach.StateMachine.add('SELECT_FINAL_POSE', select_base_pose("final_pose"),
-            transitions={'succeeded':'MOVE_ARM_TO_INIT'})
-
-        #smach.StateMachine.add('MOVE_ARM_TO_ZERO', move_arm("zeroposition"),
-        #    transitions={'succeeded':'MOVE_ARM_TO_INIT'})
-
         smach.StateMachine.add('MOVE_ARM_TO_INIT', move_arm("initposition", do_blocking = False),
             transitions={'succeeded':'APPROACH_EXIT_POSE'})
         
