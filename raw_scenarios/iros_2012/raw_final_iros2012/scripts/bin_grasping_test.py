4f7c4b949b361c6baad00ab7c9dd9c5a1dd22d8f
youbot-hbrs2-pc1 <youbot-hbrs2-pc1@h-brs.de>
2012-10-11 03:09:38 +0200

    add additional userdata required by some state

--- raw_scenarios/iros_2012/raw_final_iros2012/scripts/bin_grasping_test.py
+++ raw_scenarios/iros_2012/raw_final_iros2012/scripts/bin_grasping_test.py
@@ -33,6 +33,7 @@ if __name__ == '__main__':
     SM_BINS.userdata.goal_pose = 0
     SM_BINS.userdata.rear_platform_free_poses = ['platform_centre']
     SM_BINS.userdata.rear_platform_occupied_poses = []
+    SM_BINS.userdata.object_to_grasp = []
 
     with SM_BINS:
         smach.StateMachine.add('MOVE_ARM_OUT_OF_VIEW', move_arm_out_of_view(),
